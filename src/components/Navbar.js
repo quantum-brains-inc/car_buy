31097a0d1c90ef9398bf905946b656c51ad689a9
yacine <yacinebenz3@gmail.com>
2019-01-01 20:39:53 +0000

    removed header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ class Navbar extends Component {
     render() {
         return (
             <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        < nav class = "navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top" >
         <div  class="container">
                 <a class="navbar-brand" href="#">Annonce Auto</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
