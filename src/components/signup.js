73354ba870794d95dfda0439bc96d113229260f3
yacine <yacinebenz3@gmail.com>
2019-01-04 13:02:25 +0000

    added social share twiter

---
+++ src/components/signup.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+export default Signup;
